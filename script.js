1277701cfc20c8e3db719122c9f10aaeb0250dc7
I-am-Craz <thhiffxuui@gmail.com>
2021-06-24 22:02:13 +0600

    ref: add an edit and saving tasks in local storage

--- script.js
+++ script.js
@@ -2,32 +2,89 @@ const input = document.getElementById("add__task");
 const list = document.getElementById("content__list");
 let amountOfAllTasks = document.getElementById("total");
 
-input.onkeydown = async function (e){
+let tasksArr = [];
+
+if(localStorage.getItem('tasks')){
+    tasksArr = JSON.parse(localStorage['tasks']);
+    addFromLocalStorage(JSON.parse(localStorage['tasks']));
+    interactWithTasks();
+}
+
+input.onkeydown = function (e){
     if(e.keyCode === 13 && input.value !== ""){
-        let listItem = `
-        <div  class="list-item">
-            <h3 class="list-item__text">${input.value}</h3>
-            <span class="material-icons-outlined list-item__remove-icon">highlight_off</span>
-        </div>`;
-        amountOfAllTasks.value = list.children.length+=1;
-        list.innerHTML += listItem;
-        input.value = "";
+        tasksArr.push(input.value);
+        localStorage['tasks'] = JSON.stringify(tasksArr);
+        addTask(input.value);
     }
+   interactWithTasks();
+}
+
+function interactWithTasks(){
     const removeIcons = document.querySelectorAll('.list-item__remove-icon');
-    const listItems = document.querySelectorAll('.list-item');
-    for(let elem of listItems){
+    const tasks = document.querySelectorAll('.list-item__text');
+    const editIcons = document.querySelectorAll('.list-item__edit-icon');
+    markAsDone(tasks);
+    removeTask(removeIcons);
+    editTask(editIcons);
+}
+
+function addTask(task){
+    let listItem = `
+        <div  class="list-item">
+            <p class="list-item__text">${task}</p>
+            <div>
+                <span class="material-icons-outlined list-item__edit-icon"> mode_edit</span>
+                <span class="material-icons-outlined list-item__remove-icon">highlight_off</span>
+            </div>
+        </div>`;
+    amountOfAllTasks.value = list.children.length+=1;
+    list.innerHTML += listItem;
+    input.value = "";
+}
+
+function addFromLocalStorage(tasks){
+    for(let elem of tasks){
+        addTask(elem);
+    }
+}
+
+function editTask(arr){
+    for(let elem of arr){
+        elem.onclick = () => {
+            input.value = elem.parentNode.parentNode.children[0].textContent;
+            elem.parentNode.parentNode.remove();
+            for(let i = 0; i < tasksArr.length; i++){
+                if(tasksArr[i] === elem.parentNode.parentNode.children[0].textContent){
+                    tasksArr.splice(i, 1);
+                    localStorage.setItem('tasks', JSON.stringify(tasksArr));
+                }
+            }
+        }
+    }
+}
+
+function markAsDone(arr){
+    for(let elem of arr){
         elem.addEventListener('click', () => {
-            elem.children[0].classList.add('list-item_done');
+            elem.classList.add('list-item_done');
         });
     }
-    for(let elem of removeIcons){
+}
+
+function removeTask(arr){
+    for(let elem of arr){
         elem.onclick = () => {
-            elem.parentNode.classList.add('list-item_remove');
+            for(let i = 0; i < tasksArr.length; i++){
+                if(tasksArr[i] === elem.parentNode.parentNode.children[0].textContent){
+                    tasksArr.splice(i, 1);
+                    localStorage.setItem('tasks', JSON.stringify(tasksArr));
+                }
+            }
+            elem.parentNode.parentNode.classList.add('list-item_remove');
             setTimeout( () => {
-                elem.parentNode.remove();
+                elem.parentNode.parentNode.remove();
             }, 1000);
             amountOfAllTasks.value--;
         }
     }
 }
-
